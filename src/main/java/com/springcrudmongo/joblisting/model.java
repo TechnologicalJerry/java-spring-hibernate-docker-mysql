b9a0ad510040946123d8cc9b6219cb00c254791a
technologicaljerry <cserajatverma@gmail.com>
2023-05-16 22:41:34 +0530

    Update model.java

--- src/main/java/com/springcrudmongo/joblisting/model.java
+++ src/main/java/com/springcrudmongo/joblisting/model.java
@@ -1,9 +1,10 @@
-package com.springcrudmongo.joblisting;
+package main.java.com.springcrudmongo.joblisting;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Arrays;
 
+@Document(collection = "JobPost")
 public class model {
 
 }
