24e87c92b58229863e05eb0b1eccaf186d3f9ada
technologicaljerry <cserajatverma@gmail.com>
2023-05-16 22:31:59 +0530

    Update JoblistingApplication.java

--- src/main/java/com/springcrudmongo/joblisting/JoblistingApplication.java
+++ src/main/java/com/springcrudmongo/joblisting/JoblistingApplication.java
@@ -10,4 +10,11 @@ public class JoblistingApplication {
 		SpringApplication.run(JoblistingApplication.class, args);
 	}
 
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.withClassAnnotation(RestController.class)).paths(PathSelectors.any())
+				.build().apiInfo(apiInfo()).useDefaultResponseMessages(false);
+	}
+
 }
