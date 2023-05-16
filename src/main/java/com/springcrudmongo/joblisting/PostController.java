93ecfb634d486868403c3375375c49d3a7d98c10
technologicaljerry <cserajatverma@gmail.com>
2023-05-16 22:38:53 +0530

    Update PostController.java

--- src/main/java/com/springcrudmongo/joblisting/PostController.java
+++ src/main/java/com/springcrudmongo/joblisting/PostController.java
@@ -15,5 +15,34 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:3000")
 
 public class PostController {
+    @Autowired
+    PostRepository repo;
 
+    @Autowired
+    SearchRepository srepo;
+
+    @ApiIgnore
+    @RequestMapping(value = "/")
+    public void redirect(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/swagger-ui.html");
+    }
+
+    @GetMapping("/allPosts")
+    @CrossOrigin
+    public List<Post> getAllPosts() {
+        return repo.findAll();
+    }
+
+    // posts/java
+    @GetMapping("/posts/{text}")
+    @CrossOrigin
+    public List<Post> search(@PathVariable String text) {
+        return srepo.findByText(text);
+    }
+
+    @PostMapping("/post")
+    @CrossOrigin
+    public Post addPost(@RequestBody Post post) {
+        return repo.save(post);
+    }
 }
